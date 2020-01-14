02291c6a69cdc7e8f15c4321e025d618e2eb8732
FallenGravity <ethanvb55@gmail.com>
2020-01-14 16:17:47 +0200

    Test 2

--- apps/indexer/config/prod/ganache.exs
+++ apps/indexer/config/prod/ganache.exs
@@ -10,7 +10,7 @@ config :indexer,
       ),
     transport_options: [
       http: EthereumJSONRPC.HTTP.HTTPoison,
-      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "http://localhost:7545",
+      url: System.get_env("ETHEREUM_JSONRPC_HTTP_URL") || "http://127.0.0.1:8547",
       http_options: [recv_timeout: :timer.minutes(1), timeout: :timer.minutes(1), hackney: [pool: :ethereum_jsonrpc]]
     ],
     variant: EthereumJSONRPC.Ganache
@@ -19,6 +19,6 @@ config :indexer,
     transport: EthereumJSONRPC.WebSocket,
     transport_options: [
       web_socket: EthereumJSONRPC.WebSocket.WebSocketClient,
-      url: System.get_env("ETHEREUM_JSONRPC_WS_URL") || "ws://localhost:7545"
+      url: System.get_env("ETHEREUM_JSONRPC_WS_URL") || "ws://127.0.0.1:8548"
     ]
   ]
